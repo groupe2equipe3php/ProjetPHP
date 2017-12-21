7bc04a036d984ec9fb470972fa35650bd6fcffc7
Yoan <yoan.giovacchini@etu.univ-amu.fr>
2017-12-21 14:48:11 +0100

    Ajout d'un début de main menu

--- vue/index.php
+++ vue/index.php
@@ -1,4 +1,8 @@
 <?php
-echo 'IUT Aix-Marseille AMU, Département Informatique site Aix' . PHP_EOL;
-echo 'Groupe 2 Équipe 3 Projet PHP';
+include '../utils.inc.php';
+start_page('Traducteur Groupe2Equipe3PHP')
 ?>
+<form action="../controleur/controleur_inscription.php" method="post" xmlns="http://www.w3.org/1999/html">
+    Login : <input type="submit" name = "Login" value="Login"/></br>
+</form>
+<?php end_page(); ?>
