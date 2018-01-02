c4eb2907156ef900cd27aef663adac36d3ff39d5
Yoan <yoan.giovacchini@etu.univ-amu.fr>
2018-01-02 18:52:26 +0100

    Amélioration de la gestion des inscriptions V.2.0

--- utils.inc.php
+++ utils.inc.php
@@ -1,9 +1,10 @@
 <?php
 function start_page($title)
 {
-    echo ' <!DOCTYPE html> <html
-    lang="fr"><head><title>' . PHP_EOL . $title . '</title></head><body>' . PHP_EOL;
+    echo '<!DOCTYPE html><html lang="fr"><head><meta charset="UTF-8"/><title>'
+        . PHP_EOL . $title . '</title></head><body>' . PHP_EOL;
 };
+
 function end_page()
 {
     echo '</body></html>';
--- vue/inscription.php
+++ vue/inscription.php
@@ -2,17 +2,16 @@
 require_once '../utils.inc.php';
 start_page('Inscription');
 ?>
-<form action="../controleur/user_inscription.php" method="post">
-    <label name="nom">Nom : <input type="text" name="nom"/></label><br/>
-    <label name="prenom">Prenom : <input type="text" name="prenom"/></label><br/>
-    <label name="pseudo">Pseudo : <input type="text" name="pseudo"/></label><br/>
-    <label name="email">E-mail : <input type="text" name="email"/></label><br/>
-    <label name="mdp">Mot de passe : <input type="password" name="mdp"/></label><br/>
-    <label name="mdpverif">Vérification mot de passe : <input type="password" name="mdpverif"/></label><br/>
-    <input type="submit" value="Valider"/>
-</form>
+    <form action="../controleur/user_inscription.php" method="post">
+        <label name="nom">Nom : <input type="text" name="nom"/></label><br/><br/>
+        <label name="prenom">Prenom : <input type="text" name="prenom"/></label><br/><br/>
+        <label name="pseudo">Pseudo : <input type="text" name="pseudo"/></label><br/><br/>
+        <label name="email">E-mail : <input type="text" name="email"/></label><br/><br/>
+        <label name="mdp">Mot de passe : <input type="password" name="mdp"/></label><br/><br/>
+        <input type="submit"/><br/><br/>
+    </form>
     <form action="../vue/index.php">
-        <input type="submit" value="Accueil"/><br/>
+        <input type="submit" value="Accueil"/>
     </form>
 <?php
 end_page();
