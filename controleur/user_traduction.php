ce73eebdd266a7bffeda29830e3a694c12971db6
Yoan <yoan.giovacchini@etu.univ-amu.fr>
2018-01-17 09:26:34 +0100

    Commencement de user_traduction

--- controleur/user_traduction.php
+++ controleur/user_traduction.php
@@ -2,12 +2,12 @@
 session_start();
 require_once '../gettext.inc.php';
 require_once '../utils.inc.php';
-require_once '../bdd_connexion.php';
+require_once  '../vue/traduction.php';
+initialiser_gettext($SESSION['lang']);
 
-$bdd = bdd_connexion();
+$MAT = htmlspecialchars($_POST['MAT']);
 
-if (is_null($bdd)) {
-    echo _("Vous n'êtes pas connecté veulliez vous connecter pour pouvoir traduire");
-}
+echo _($MAT);
+
 end_page();
 ?>
