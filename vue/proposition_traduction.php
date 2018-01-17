4253f3bfc08a91bc421916c27a829c7e1080f474
Yoan <yoan.giovacchini@etu.univ-amu.fr>
2018-01-17 10:46:50 +0100

    Création d'un contrôleur pour la vue proposer_traduction

--- vue/proposition_traduction.php
+++ vue/proposition_traduction.php
@@ -1,3 +1,21 @@
 <?php
 session_start();
-echo 'TODO';
+require_once '../gettext.inc.php';
+require_once '../utils.inc.php';
+
+initialiser_gettext($_SESSION['lang']);
+start_page(_("Inscription"));
+?>
+<?php echo _("Ce mot n'est pas traduit, voulez vous le traduire ?") ?><br/><br/>
+    <form action="../controleur/user_traduction.php" method="post">
+            <label name="traduction"><?php echo _("Traduction ") ?>
+                <input type="text" name="mot_a_traduire"/></label><br/><br/>
+            <input type="submit" value="<?php echo _("Traduire") ?>"/>
+    </form><br/>
+
+    <form action="../vue/index.php">
+        <input type="submit" value="<?php echo _("Accueil") ?>"/>
+    </form>
+<?php
+end_page();
+?>
