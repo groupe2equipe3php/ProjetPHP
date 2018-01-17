edce207a96140771c4213c153ded0943e626b0d4
Yoan <yoan.giovacchini@etu.univ-amu.fr>
2018-01-17 09:17:16 +0100

    Vue traduction

--- controleur/user_traduction.php
+++ controleur/user_traduction.php
@@ -1,9 +1,13 @@
 <?php
 session_start();
 require_once '../gettext.inc.php';
+require_once '../utils.inc.php';
+require_once '../bdd_connexion.php';
 
-echo 'TODO<br/>';
+$bdd = bdd_connexion();
+
+if (is_null($bdd)) {
+    echo _("Vous n'êtes pas connecté veulliez vous connecter pour pouvoir traduire");
+}
+end_page();
 ?>
-    <form action="../vue/index.php">
-        <br/><input type="submit" value="<?php echo _("Accueil") ?>"/><br/>
-    </form>
